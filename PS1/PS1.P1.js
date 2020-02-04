6071265653318f322c411825ca0a5b2e779fbc5a
Juan Santana <juansantanajunior@gmail.com>
2020-02-04 04:34:12 -0500

    first file made

---
+++ PS1/PS1.P1.js
@@ -0,0 +1,5 @@
+// Problem 1 (PS1.P1.js)
+
+// DESCRIPTION: Write a function that takes a string as its input and 
+// returns a new string that contains all of the letters in the original string, 
+//but in alphabetical order. Ignore punctuation and numbers. 
