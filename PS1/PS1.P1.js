374c9d9aa76960fb3bcaa47d7467ca9ed4150e03
Juan Santana <juansantanajunior@gmail.com>
2020-02-05 17:59:00 -0500

    Done with PS1

--- PS1/PS1.P1.js
+++ PS1/PS1.P1.js
@@ -1,5 +1,20 @@
-// PS1 Problem 1 (PS1.P1.js)
+/* PS1 Problem 1 (PS1.P1.js)
+ DESCRIPTION: Write a function that takes a string as its input and
+ returns a new string that contains all of the letters in the original string,
+ but in alphabetical order. IGNORE punctuation and numbers.
+ */
 
-// DESCRIPTION: Write a function that takes a string as its input and 
-// returns a new string that contains all of the letters in the original string, 
-//but in alphabetical order. Ignore punctuation and numbers. 
+const alphabeticalOrderString = inputString => inputString.replace(/[.,@\'"/#!?$<>%\^&\*;:{}=\-_`~()1234567890]/g,"").split('').sort().join('');
+
+// Some sample tests testing the function above
+const sampleTest1 = 'supercalifragilisticexpialidocious';
+console.log(`The alphabetical order of the string: ${sampleTest1} is ${alphabeticalOrderString(sampleTest1)}`);
+/* Should print out the following:
+The alphabetical order of the string: supercalifragilisticexpialidocious is aaacccdeefgiiiiiiillloopprrssstuux
+ */
+
+const sampleTest2 = 'super123cali^fragilistic456expialidocious!?!';
+console.log(`Ignoring puncations and numbers, the alphabetical order of the string: ${sampleTest2} is ${alphabeticalOrderString(sampleTest2)}`);
+/* Should print out the following:
+Ignoring puncations and numbers, the alphabetical order of the string: super123cali^fragilistic456expialidocious!?! is aaacccdeefgiiiiiiillloopprrssstuux
+ */
