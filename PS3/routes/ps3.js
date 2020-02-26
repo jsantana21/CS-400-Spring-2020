c64776fae506ede1f3ad04fb6418ff82bf3cc31c
Juan Santana <juansantanajunior@gmail.com>
2020-02-26 13:58:28 -0500

    PS3 Done

--- PS3/routes/ps3.js
+++ PS3/routes/ps3.js
@@ -6,8 +6,7 @@ router.route('/')
         res.render('ps3', {string:"Hello World!"});
     })
     .post((req, res, next) => {
-        const postString = {OriginalString: req.body.string, OriginalStringLength: req.body.string.length};
-        res.render('ps3', postString);
+        res.render('ps3', {OriginalString: req.body.string, OriginalStringLength: req.body.string.length});
     });
 
 module.exports = router;
