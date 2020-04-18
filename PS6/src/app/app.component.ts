ff86a86e75a6f267909faa085df199ed91158c34
Juan Santana <juansantanajunior@gmail.com>
2020-04-18 17:51:17 -0400

    PS6 Done

--- PS6/src/app/app.component.ts
+++ PS6/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {WeatherDataArray} from './Weather Data MOCK';
+import {WeatherData} from './Weather Data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'PS6';
+  title = 'Weather Angular App';
+  weather = WeatherDataArray;
+  private selectedData: WeatherData;
+
+  selectData(data: WeatherData) {
+    this.selectedData = data;
+  }
 }
